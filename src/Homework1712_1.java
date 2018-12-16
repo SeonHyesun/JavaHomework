b7e10e9fdb06d5d7a2ae4749f07ffe645c0b51d8
dima <dima@192.168.10.3>
2018-12-16 15:44:52 +0900

    homework1712

---
+++ src/Homework1712_1.java
@@ -0,0 +1,17 @@
+
+public class Homework1712_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		int j=1;
+		int k=1;
+		
+		for(i=0;i<=19;i++) {
+			j=j+i;
+			k+=j;
+		}
+		System.out.println(k);
+	}
+}
+
---
+++ src/Homework1712_2.java
@@ -0,0 +1,20 @@
+
+public class Homework1712_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=InputVal.getInt();
+		int i=a-1;
+		int j=2;
+		
+		for(j=2;j<=i;j++) {
+			if(a%j){
+				System.out.print();
+			}
+	
+			
+			}
+		
+		}
+		
+	}
