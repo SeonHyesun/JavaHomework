f88ac73e0b37b0bc290c2fcef990f34e9b7482d0
dima <dima@192.168.10.3>
2018-12-18 21:47:12 +0900

    homework1912

--- src/Homework1712_2.java
+++ src/Homework1712_2.java
@@ -4,15 +4,18 @@ public class Homework1712_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=InputVal.getInt();
-		int i=a-1;
 		int j=2;
 
-		while(j<=i) {
-			if(a%j==0){
-				System.out.print("소수입니다.");
-			}else
-				System.out.println("소수가 아닙니다.");
-
+		boolean isPrime=true;
+		for(j=2;j<=a;j++) {
+			if(a%j==0) {
+				isPrime=false;
+				break;
+			}
 		}
+		if(isPrime) {
+			System.out.print(a+"는 소수입니다.");
+		}else
+		System.out.println(a+"는 소수가 아닙니다.");
 	}
 }
---
+++ src/Homework1912_2.java
@@ -0,0 +1,20 @@
+
+public class Homework1912_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int b=InputVal.getInt();
+		int mok=12;
+		int nmg=1;
+		int c=0;
+		
+		int cArr[]=new int[c.length()];
+		for(c=0;c<=cArr.length;c++) {
+			mok=b/2;
+			cArr[]=b%mok
+			
+		}		
+
+	}
+
+}
